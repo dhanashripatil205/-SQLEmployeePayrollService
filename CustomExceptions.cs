285ef741772f6afbc0776d91a2db27c88a7cfdfd
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-02-01 01:56:46 +0530

    Ability to establish connection using ADO.NET!!!

---
+++ CustomExceptions.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedAddressBookProblem
+{
+    public class CustomExceptions : Exception
+    {
+        ExceptionType type;
+        public enum ExceptionType
+        {
+            CONNECTION_FAILED
+        }
+        public CustomExceptions(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
